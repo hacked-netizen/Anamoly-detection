532e876e9abc27e105aad4870c4b28a3a3229daa
Utkarsh Swarup <76107260+hacked-netizen@users.noreply.github.com>
2022-04-01 23:01:34 +0530

    Update Test_Anomaly_Detector_public.py

--- Test_Anomaly_Detector_public.py
+++ Test_Anomaly_Detector_public.py
@@ -71,8 +71,6 @@ def load_dataset_One_Video_Features(Test_Video_Path):
     f = open(VideoPath, "r")
     words = f.read().split()
     num_feat = len(words) / 4096
-    # Number of features per video to be loaded. In our case num_feat=32, as we divide the video into 32 segments. Note that
-    # we have already computed C3D features for the whole video and divided the video features into 32 segments.
 
     count = -1;
     VideoFeatues = []
@@ -93,13 +91,12 @@ print("Starting testing...")
 
 
 AllTest_Video_Path = '/newdata/UCF_Anomaly_Dataset/Dataset/CVPR_Data/C3D_Complete_Video_txt/Test/'
-# AllTest_Video_Path contains C3D features (txt file)  of each video. Each file contains 32 features, each of 4096 dimensions.
+
 Results_Path = '../Eval_Res/'
-# Results_Path is the folder where you can save your results
+
 Model_dir='../Trained_AnomalyModel/'
-# Model_dir is the folder where we have placed our trained weights
+
 weights_path = Model_dir + 'weights_L1L2.mat'
-# weights_path is Trained model weights
 
 model_path = Model_dir + 'model.json'
 
@@ -117,11 +114,11 @@ time_before = datetime.now()
 for iv in range(nVideos):
 
     Test_Video_Path = os.path.join(AllTest_Video_Path, All_Test_files[iv])
-    inputs=load_dataset_One_Video_Features(Test_Video_Path) # 32 segments features for one testing video
-    predictions = model.predict_on_batch(inputs)   # Get anomaly prediction for each of 32 video segments.
+    inputs=load_dataset_One_Video_Features(Test_Video_Path) 
+    predictions = model.predict_on_batch(inputs)  
     aa=All_Test_files[iv]
     aa=aa[0:-4]
-    A_predictions_path = Results_Path + aa + '.mat'  # Save array of 1*32, containing anomaly score for each segment. Please see Evaluate Anomaly Detector to compute  ROC.
+    A_predictions_path = Results_Path + aa + '.mat'  
     print "Total Time took: " + str(datetime.now() - time_before)
 
 
